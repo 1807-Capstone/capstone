89a883ec274464a13cfe7de3936d78d1ecf8a115
abby wigdale <abby.wigdale@gmail.com>
2018-09-28 15:13:17 -0500

    yelp-api-testing

--- server/api/restaurants.js
+++ server/api/restaurants.js
@@ -1,22 +1,63 @@
 const router = require('express').Router()
-const axios = require('axios')
 const yelp = require('yelp-fusion')
 module.exports = router
 
 const client = yelp.client(process.env.YELP_FUSION_API)
+const googleMapsClient = require('@google/maps').createClient({
+  key: process.env.GOOGLE_MAPS_API,
+  Promise: Promise
+})
 
-router.get('/yelp', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    const search = await client.search({
+    //Google search
+    const initialGoogleSearch = await googleMapsClient
+      .placesNearby({
+        language: 'en',
+        location: [41.895579, -87.639064],
+        radius: 1000,
+        minprice: 1,
+        maxprice: 4,
+        type: 'restaurant'
+      })
+      .asPromise()
+    const googleSearch = initialGoogleSearch.json.results
+    console.log('this is google info', googleSearch[0])
+
+    // const finalSearch = googleSearch.map(async googleObj => {
+    //   try {
+    //     console.log('this is google object', googleObj.name)
+    //     const initialYelpSearch = await client.search({
+    //       term: googleObj.name,
+    //       address1: googleObj.vicinity.split(',')[0],
+    //       latitude: googleObj.geometry.location.lat,
+    //       longitude: googleObj.geometry.location.lng,
+    //       limit: 1
+    //     })
+    //     const yelpSearchResult = initialYelpSearch.jsonBody.businesses
+    //     console.log('this is yelp search result', yelpSearchResult[0].name)
+    //     //return {...googleObj, yelpRating: yelpSearchResult[0].rating}
+    //   } catch (err) {
+    //     console.error(err)
+    //   }
+    // })
+    //res.status(200).json(finalSearch)
+
+    //Yelp search
+    const initialYelpSearch = await client.search({
       term: 'food',
-      latitude: 41.9,
-      longitude: -87.7,
-      radius: 10000,
-      limit: 40
+      latitude: 41.895579,
+      longitude: -87.639064,
+      radius: 1000
     })
-    const all = search.jsonBody.businesses
-    res.status(200).json(all)
+    const yelpSearch = initialYelpSearch.jsonBody.businesses
+    console.log('this is yelp info', yelpSearch[0])
   } catch (err) {
-    next(err)
+    console.error(err)
   }
 })
+
+// term: 'Volare Ristorante Italiano',
+// latitude: 41.8914882,
+// longitude: -87.6225091,
+// limit: 1

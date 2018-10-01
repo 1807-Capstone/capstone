515aedc87bff0cdb98cd227e9eeac3bdd29c9cbd
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-10-01 11:36:27 -0500

    adds local state for map

--- client/components/map.js
+++ client/components/map.js
@@ -1,81 +1,103 @@
 import React from 'react'
 import mapboxgl from 'mapbox-gl'
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
+import {setLocation} from '../store/map'
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA'
 
-export class MapView extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 15
-    }
+let map
+const mapDispatchToProps = dispatch => {
+  return {
+    setLocation: location => dispatch(setLocation(location))
   }
+}
 
+const mapStateToProps = state => {
+  return {
+    location: state.map.location,
+    markers: state.map.markers
+  }
+}
+
+export class MapView extends React.Component {
   componentDidMount() {
-    const {lng, lat, zoom} = this.state
-
-    const map = new mapboxgl.Map({
+    console.log('componentdidmount', this.props)
+    map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v9',
-      center: [lng, lat],
-      zoom
+      center: [this.props.location.lng, this.props.location.lat],
+      zoom: this.props.location.zoom
     })
+    this.geolocate()
+  }
 
-    map.on('move', () => {
-      const {lng, lat} = map.getCenter()
-
-      this.setState({
-        lng: lng.toFixed(4),
-        lat: lat.toFixed(4),
-        zoom: map.getZoom().toFixed(2)
-      })
-    })
-
-    map.on('load', function() {
-      map.addLayer({
-        id: 'terrain-data',
-        type: 'line',
-        source: {
-          type: 'vector',
-          url: 'mapbox://mapbox.mapbox-terrain-v2'
-        },
-        'source-layer': 'contour'
-      })
-    })
-
+  geolocate() {
     const geolocate = new mapboxgl.GeolocateControl({
       positionOptions: {
         enableHighAccuracy: true
       },
       trackUserLocation: true
     })
+
     map.addControl(geolocate)
     setTimeout(() => geolocate.trigger(), 1000)
-    let mark = document.createElement('div')
-    mark.className = 'marker'
-    new mapboxgl.Marker(mark).setLngLat([-87.639, 41.8956]).addTo(map)
-    // new mapboxgl.LngLat(lng, lat).toBounds(5000)
-    console.log('test:', map.getBounds())
+    map.on('move', () => {
+      const {lng, lat} = map.getCenter()
+
+      this.props.setLocation({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      })
+    })
+  }
+  createMarker(lng, lat, className) {
+    var marker = document.createElement('div')
+    marker.className = className
+    new mapboxgl.Marker(marker).setLngLat([lng, lat]).addTo(map)
+  }
+
+  addLayer(obj) {
+    map.on('load', function() {
+      map.addLayer(obj)
+      //obj example
+      // {
+      //   id: 'terrain-data',
+      //   type: 'line',
+      //   source: {
+      //     type: 'vector',
+      //     url: 'mapbox://mapbox.mapbox-terrain-v2'
+      //   },
+      //   'source-layer': 'contour'
+      // }
+    })
   }
 
   render() {
-    const {lng, lat, zoom} = this.state
-
-    return (
-      <div>
-        <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
-          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+    if (this.props.location) {
+      return (
+        <div>
+          <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
+            <div>{`Longitude: ${this.props.location.lng} Latitude: ${
+              this.props.location.lat
+            } Zoom: ${this.props.location.zoom}`}</div>
+          </div>
+          <div
+            ref={el => (this.mapContainer = el)}
+            className="absolute top right left bottom"
+          />
         </div>
-        <div
-          ref={el => (this.mapContainer = el)}
-          className="absolute top right left bottom"
-        />
-      </div>
-    )
+      )
+    } else {
+      return null
+    }
   }
 }
 
-export default MapView
+const ConnectedMapView = withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(MapView)
+)
+
+export default ConnectedMapView

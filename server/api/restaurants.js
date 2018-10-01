ee63bcdfb3421aba071e36b6951f8dc16bc76093
awigdale <38301991+awigdale@users.noreply.github.com>
2018-10-01 14:28:07 -0500

    Update restaurants.js

--- server/api/restaurants.js
+++ server/api/restaurants.js
@@ -115,37 +115,7 @@ router.get('/', async (req, res, next) => {
 
     // console.log('** Done!')
 
-    // let yelpResults = 'hi'
-    // console.log('yelp query length: ', yelpQuery)
-
-    // const finalSearch = googleSearch.map(async googleObj => {
-    //   try {
-    //     console.log('this is google object', googleObj.name)
-    //     const initialYelpSearch = await client.search({
-    //       term: googleObj.name,
-    //       address1: googleObj.vicinity.split(',')[0],
-    //       latitude: googleObj.geometry.location.lat,
-    //       longitude: googleObj.geometry.location.lng,
-    //       limit: 1
-    //     })
-    //     const yelpSearchResult = initialYelpSearch.jsonBody.businesses
-    //     console.log('this is yelp search result', yelpSearchResult[0].name)
-    //     //return {...googleObj, yelpRating: yelpSearchResult[0].rating}
-    //   } catch (err) {
-    //     console.error(err)
-    //   }
-    // })
-    //res.status(200).json(finalSearch)
-
-    //Yelp search
-    // const initialYelpSearch = await client.search({
-    //   term: 'food',
-    //   latitude: 41.895579,
-    //   longitude: -87.639064,
-    //   radius: 1000
-    // })
-    // const yelpSearch = initialYelpSearch.jsonBody.businesses
-    // console.log('this is yelp info', yelpSearch[0])
+   
   } catch (err) {
     console.error(err)
   }

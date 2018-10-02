e8c24d2464e1c47f8afde3be0fdaacd5dc89f1c7
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-10-02 18:26:04 -0500

    cleans up commented out code

--- client/components/map.js
+++ client/components/map.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import mapboxgl from 'mapbox-gl'
-// var MapboxDirections = require('@mapbox/mapbox-gl-directions')
 import {fetchAllRestaurantsFromServer} from '../store/restaurant'
 import {connect} from 'react-redux'
 
@@ -45,13 +44,6 @@ export class MapView extends React.Component {
       zoom: this.props.location.zoom
     })
     await this.geolocate()
-    // var directions = new MapboxDirections({
-    //   accessToken:
-    //     'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA',
-    //   unit: 'metric'
-    //   // profile: 'mapbox/cycling'
-    // })
-    // map.addControl(directions, 'top-left')
     map.addControl(
       new MapboxDirections({
         accessToken: mapboxgl.accessToken

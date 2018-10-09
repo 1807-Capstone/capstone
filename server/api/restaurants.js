602b7355e54d24260c54bbc3b185e3f221d255c7
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-10-09 16:44:33 -0500

    queries distance, needs calculation

--- server/api/restaurants.js
+++ server/api/restaurants.js
@@ -10,6 +10,7 @@ const {
   yelpQueryMaker
 } = require('../../utils');
 const {Restaurant} = require('../db/models');
+const Sequelize = require('sequelize');
 module.exports = router;
 
 // const client = yelp.client(process.env.YELP_FUSION_API);
@@ -260,13 +261,21 @@ router.post('/yelp', async (req, res, next) => {
 });
 
 router.post('/filteredServer', async (req, res, next) => {
+  const Op = Sequelize.Op;
+  const [latitude, longitude] = [41.8941717, -87.6345914];
+  const currentFilters = {};
+
   try {
-    const currentFilters = {};
     if (req.body.price) {
       currentFilters.price_level = req.body.price;
     }
-    if (req.body.rating) {
-      currentFilters.radiusRating = req.body.rating;
+    if (req.body.distance) {
+      currentFilters.lat = {
+        [Op.between]: [latitude - 1, latitude + 1]
+      };
+      currentFilters.lng = {
+        [Op.between]: [longitude - 1, longitude + 1]
+      };
     }
     if (req.body.cuisine) {
       currentFilters.keyword = req.body.cuisine;
@@ -279,35 +288,6 @@ router.post('/filteredServer', async (req, res, next) => {
     next(error);
   }
 });
-
-// router.post('/allRestaurants', async (req, res, next) => {
-//   const Op = Sequelize.Op;
-//   try {
-//     const allRestaurants = await Restaurant.findAll({
-//       where: {
-//         // location: [
-//         //   {
-//         //     [Op.or]: {
-//         //       [Op.lt]: req.body.lat + 1,
-//         //       [Op.gt]: req.body.lat - 1
-//         //     },
-//         //     [Op.or]: {
-//         //       [Op.lt]: req.body.lng + 1,
-//         //       [Op.gt]: req.body.lng - 1
-//         //     }
-//         //   }
-//         // ]
-//         location: [
-//           {[Op.between]: [req.body.lat + 1, req.body.lat - 1]},
-//           {[Op.between]: [req.body.lng + 1, req.body.lng - 1]}
-//         ]
-//       }
-//     });
-//     res.json(allRestaurants);
-//   } catch (err) {
-//     next(err);
-//   }
-// });
 
 router.get('/:id', async (req, res, next) => {
   try {

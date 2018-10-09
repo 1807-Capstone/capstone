5a7c4decb5f92351549afcf2bdad8518651cdceb
Juliet Welcome <juliet.welcome@gmail.com>
2018-10-09 11:40:46 -0500

    back button from single restaurant view

--- client/components/singleRestaurant.js
+++ client/components/singleRestaurant.js
@@ -34,57 +34,67 @@ export class SingleRestaurant extends Component {
     this.props.triggerCheckIn(this.props.user);
   };
 
+  static contextTypes = {
+    router: () => true
+  };
+
   render() {
     const restaurant = this.props.restaurant;
     if (restaurant) {
       return (
-        <Grid stackable>
-          <Grid.Column computer={5} mobile={10}>
-            <Image src={restaurant.yelpResults.image_url} />
-            <StyledTitle>{restaurant.name}</StyledTitle>
-            <br />
-            <Button basic onClick={this.handleCheckIn}>
-              Check In Here
-            </Button>
-            <br />
-            <div>
-              Radius Rating:
-              <ReactStars
-                count={5}
-                value={restaurant.radiusRating}
-                half={true}
-                edit={false}
-                color2="#35b3bf"
-                // size="25px"
-              />
-            </div>
-            <div>
-              Yelp Rating:{' '}
-              <ReactStars
-                count={5}
-                value={restaurant.yelpResults.rating}
-                half={true}
-                edit={false}
-                color2="#C50A00"
-              />
-            </div>
-            <div>
-              Google Rating:{' '}
-              <ReactStars
-                count={5}
-                value={restaurant.rating}
-                half={true}
-                edit={false}
-                color2="#C58600"
-              />
-            </div>
-            <p>Price Level:{restaurant.price_level}</p>
-            <p>Address: {restaurant.vicinity}</p>
-          </Grid.Column>
-          <Grid.Column computer={6} mobile={10}>
-            <SingleRestaurantMap />
-          </Grid.Column>
-        </Grid>
+        <div>
+          <Button basic fluid onClick={this.context.router.history.goBack}>
+            Go Back
+          </Button>
+          <br />
+          <Grid stackable>
+            <Grid.Column computer={5} mobile={10}>
+              <Image src={restaurant.yelpResults.image_url} />
+              <StyledTitle>{restaurant.name}</StyledTitle>
+              <br />
+              <Button basic onClick={this.handleCheckIn}>
+                Check In Here
+              </Button>
+              <br />
+              <div>
+                Radius Rating:
+                <ReactStars
+                  count={5}
+                  value={restaurant.radiusRating}
+                  half={true}
+                  edit={false}
+                  color2="#35b3bf"
+                  // size="25px"
+                />
+              </div>
+              <div>
+                Yelp Rating:{' '}
+                <ReactStars
+                  count={5}
+                  value={restaurant.yelpResults.rating}
+                  half={true}
+                  edit={false}
+                  color2="#C50A00"
+                />
+              </div>
+              <div>
+                Google Rating:{' '}
+                <ReactStars
+                  count={5}
+                  value={restaurant.rating}
+                  half={true}
+                  edit={false}
+                  color2="#C58600"
+                />
+              </div>
+              <p>Price Level:{restaurant.price_level}</p>
+              <p>Address: {restaurant.vicinity}</p>
+            </Grid.Column>
+            <Grid.Column computer={6} mobile={10}>
+              <SingleRestaurantMap />
+            </Grid.Column>
+          </Grid>
+        </div>
       );
     } else return <h3>Sorry, we could not find this restaurant</h3>;
   }
